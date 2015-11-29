af9898cf27de98f4e7977e9ab0bddaa6e5987ab3
H4jen <alterweggo2@gmail.com>
2015-11-29 02:49:04 +0100

    cagame32.h and pattern added

---
+++ pattern_files/qagame32.h
@@ -0,0 +1,6 @@
+// qagame32.h #define CLIENTSPAWN #define G_RUNFRAME  #define CHECKPRIVILEGES #define GETCLIENTNAME 
+#define G_RUNFRAME                  0x00082270
+#define G_ADDEVENT                  0x00098EF0
+#define G_INITGAME                  0x0007F110    
+#define CLIENTCONNECT               0x0005ECD0
+#define CLIENTDISCONNECT            0x00060FD0
